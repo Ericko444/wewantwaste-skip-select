2dec7ef29d0a03053402d090a36a34ed4b184859
Ericko Manarivo <manarivoericko4@gmail.com>
2025-05-10 18:13:00 +0300

    feat(ui): updated main layout display

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -8,12 +8,14 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     return (
-        <div>
-            <Header />
-            <main>
-                {children}
-            </main>
-            <Footer />
+        <div className="bg-gray-100 min-h-screen text-gray-800 font-sans">
+            <div className="container mx-auto px-4 py-8 relative z-10">
+                <Header />
+                <main className="flex-grow">
+                    {children}
+                </main>
+                <Footer />
+            </div>
         </div>
     );
 };
